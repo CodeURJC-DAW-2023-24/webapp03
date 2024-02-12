7fa2d4b2d135c2952ab2cae7f9b58a10fac0fec0
Izan Ruiz Ballesteros <117302436+Etheko@users.noreply.github.com>
2024-02-12 12:23:45 +0100

    First integration of Spring + Files Moved
    First attempt to implement SpringBoot to the project.
    
    The files have been moved to follow the required file-system structure.

---
+++ src/main/java/es/codeurjc/holamundo/Webapp03Application.java
@@ -0,0 +1,14 @@
+package es.codeurjc.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webapp03Application.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/es/codeurjc/holamundo/Webapp03ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
