582fe008950af43343dc1fe92e6f84bdd0443630
DiRoGa <peromarthegamer64@gmail.com>
2024-02-20 20:14:52 +0100

    Added "modifyDone()" method
    Added new "modifyDone()" method to update user info into the database.

--- src/main/java/es/codeurjc/holamundo/controller/EditProfileController.java
+++ src/main/java/es/codeurjc/holamundo/controller/EditProfileController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class EditProfileController {
+    private User userInfo;
     private UserList users;
+    private UserRepository userBD;
 
     public EditProfileController() {
         this.users = new UserList();
@@ -16,6 +18,15 @@ public class EditProfileController {
 
     @GetMapping("/profile/{username}/edit")
     public String loadEditProfilePage(Model model, @PathVariable String username) {
+        this.userInfo = userBD.findByUsername();
+
+        /*
+         * if (userInfo.isPresent()) {
+         * return ResponseEntity.ok(userInfo.get());
+         * } else {
+         * return ResponseEntity.of(userInfo);
+         * }
+         */
 
         String alias = users.getUserInfo(username)[1];
         String role = users.getUserInfo(username)[2];
@@ -33,5 +44,29 @@ public class EditProfileController {
         model.addAttribute("password", password);
 
         return "editProfilePage";
+    }
+
+    @PutMapping("/modifyDone/{username}") // Parametros del formulario
+    public String modifyDone(Model model, @PathVariable("inputUsername") String inputUsername,
+            @RequestParam("inputAlias") String inputAlias,
+            @RequestParam("inputBookAuthorName") String inputBookAuthorName,
+            @RequestParam("inputBookISBN") String inputBookISBN, @RequestParam("inputRole") String inputRole,
+            @RequestParam("inputDescription") String inputDescription,
+            @RequestParam("inputProfileImage") String inputProfileImage, @RequestParam("inputEmail") String inputEmail,
+            @RequestParam("inputPassword") String inputPassword) {
+               
+        // Se puede realizar con setter en la clase Book
+        users.setUserInfo(username, inputAlias + inputBookAuthorName + inputBookISBN + inputRole + inputDescription
+                + inputProfileImage + inputEmail + inputPassword);
+
+        // Tambien se puede crear una clase Book y sobreescribir con updateBook y el id
+        // Book newBook = new Book(bookID, inputBookName, inputBookAuthorName,
+        // inputBookDescription, inputBookDate, inputBookDescription, inputBookISBN,
+        // inputBookGenre, inputBookSeries, inputBookPages, inputBookPublisher);
+        // books.updateBook(bookID, newBook);
+
+        userBD.replace(users.getUser(username));
+
+        return "redirect:/profile/" + username;
     }
 }
