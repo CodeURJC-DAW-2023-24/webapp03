1f7ce232e560d0b7db0214147c4231307f1d49f4
Blas Vita Ramos <b.vita.2020@alumnos.urjc.es>
2024-02-23 01:22:41 +0100

    Profile edition now works with database, revamped page
    Profile modification now updates the database (password change is still subject to change due to security reasons).
    
    Small changes to the page structure and upgraded UX.

--- src/main/java/es/codeurjc/holamundo/controller/EditProfileController.java
+++ src/main/java/es/codeurjc/holamundo/controller/EditProfileController.java
@@ -1,14 +1,28 @@
 package es.codeurjc.holamundo.controller;
 
+import es.codeurjc.holamundo.entity.User;
+import es.codeurjc.holamundo.repository.UserRepository;
 import es.codeurjc.holamundo.service.UserList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EditProfileController {
     private UserList users;
+
+    @Autowired
+    UserRepository userRepository;
 
     public EditProfileController() {
         this.users = new UserList();
@@ -17,8 +31,8 @@ public class EditProfileController {
     @GetMapping("/profile/{username}/edit")
     public String loadEditProfilePage(Model model, @PathVariable String username) {
 
-        String alias = users.getUserInfo(username)[1];
-        String role = users.getUserInfo(username)[2];
+        String alias = users.getUserInfo(username)[2];
+        String role = users.getUserInfo(username)[1];
         String description = users.getUserInfo(username)[3];
         String profileImage = users.getUserInfo(username)[4];
         String email = users.getUserInfo(username)[5];
@@ -33,5 +47,46 @@ public class EditProfileController {
         model.addAttribute("password", password);
 
         return "editProfilePage";
+    }
+
+    @PostMapping("/profile/{username}/edit")
+    public String editProfile(Model model, @PathVariable String username,
+                              @RequestParam("alias") String newAlias,
+                              @RequestParam("email") String newEmail,
+                              @RequestParam("description") String newDescription,
+                              @RequestParam("password") String newPassword)
+    {
+
+        User user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            user.setAlias(newAlias);
+            user.setEmail(newEmail);
+            user.setDescription(newDescription);
+            user.setPassword(newPassword);
+            userRepository.save(user);
+        }
+
+        return "redirect:https://localhost:8443/profile/" + username;
+    }
+
+    @PostMapping("/profile/{username}/editPassword")
+    public ResponseEntity<?> editProfile(@RequestParam("currentPassword") String currentPassword, @RequestParam("newPassword") String newPassword) {
+        // Obtén el usuario actual
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+
+        // Compara la contraseña actual con la almacenada en la base de datos
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if (!encoder.matches(currentPassword, userDetails.getPassword())) {
+            return new ResponseEntity<>("La contraseña actual es incorrecta", HttpStatus.BAD_REQUEST);
+        }
+
+        // Encripta la nueva contraseña
+        String encodedNewPassword = encoder.encode(newPassword);
+
+        // Aquí debes implementar la lógica para actualizar la contraseña del usuario en la base de datos
+
+        return new ResponseEntity<>("Contraseña actualizada con éxito", HttpStatus.OK);
     }
 }
--- src/main/resources/static/js/editProfilePage.js
+++ src/main/resources/static/js/editProfilePage.js
@@ -1,9 +1,88 @@
 $(() => {
+    $("#passwordsNotMatching").hide();
+    $("#passwordChanged").hide();
+    $("#passwordRequirements").hide();
+    $("#confirmPasswordChange").hide();
+
+    let passwordChanged = false;
+
     $("#editProfileBut").on("click", () => {
-        window.location.assign( window.location.href + "/edit");
+        window.location.assign(window.location.href + "/edit");
+    });
+
+    $("#returnBut").on("click", () => {
+        window.location.assign(window.location.href.slice(0, -5));
+        $("#passwordChanged").hide();
     });
 
     $("#saveChangesBut").on("click", () => {
-        window.location.assign( window.location.href.slice(0, -5));
+        $.ajax({
+            url: window.location.href + "?alias=" + $("#inputAlias").val() + "&email=" + $("#inputEmailAddress").val() + "&description=" + $("#inputProfileDescription").val() + "&password=" + $("#inputPassword").val(),
+            type: "POST",
+            datatype: "json",
+            success: (data) => {
+                window.location.assign(window.location.href.slice(0, -5));
+                if (passwordChanged) {
+                    $("#passwordChanged").hide();
+                    $("#confirmPasswordChange").hide();
+                } else if ((!passwordChanged) && ($("#inputPassword").val() !== "")) {
+                    $("#confirmPasswordChange").show();
+                }
+            }
+        });
+    });
+
+    function passwordsDoMatch() {
+        setTimeout(() => {
+            let newPassword = $("#inputPassword").val();
+            let confirmPassword = $("#inputConfirmPassword").val();
+
+            if ((newPassword !== confirmPassword) && (newPassword !== "") && (confirmPassword !== "")) {
+                $("#passwordsNotMatching").show();
+            } else {
+                $("#passwordsNotMatching").hide();
+            }
+        }, 100)
+
+    }
+
+    function passwordMeetsRequirements(newPassword) {
+        let passwordRequirements = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        if (newPassword.match(passwordRequirements)) {
+            $("#passwordRequirements").hide();
+            return true;
+        } else {
+            $("#passwordRequirements").show();
+            return false;
+        }
+    }
+
+    $(".change-password-button").on("click", () => {
+        let currentHypoPassword = $("#inputOldPassword").val();
+        let newPassword = $("#inputPassword").val();
+        let confirmPassword = $("#inputConfirmPassword").val();
+
+        if ((newPassword !== "") && (newPassword === confirmPassword) && passwordMeetsRequirements(newPassword)) {
+            $.ajax({
+                url: window.location.href.replace("edit", "editPassword"),
+                type: "POST",
+                data: {
+                    currentPassword: currentHypoPassword,
+                    newPassword: newPassword
+                },
+                success: function (response) {
+                    $("#passwordChanged").show();
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        }
+    });
+
+    $("#inputPassword, #inputConfirmPassword").on("keydown", (e) => {
+        passwordsDoMatch();
+
+        passwordMeetsRequirements($("#inputPassword").val());
     });
 })
