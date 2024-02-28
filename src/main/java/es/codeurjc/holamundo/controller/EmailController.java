2da02d74090e5bc97ceb22d378f9af9826e6af81
Diego Rodriguez <peromarthegamer64@gmail.com>
2024-02-28 10:29:46 +0100

    Added Email Support
    Created "EmailController.java" and "EmailService.java" to support email service.

---
+++ src/main/java/es/codeurjc/holamundo/controller/EmailController.java
@@ -0,0 +1,27 @@
+package es.codeurjc.holamundo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.codeurjc.holamundo.service.EmailService;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send")
+    public String sendEmail(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String text) {
+
+        emailService.sendEmail(to, subject, text);
+        return "Email sent successfully!";
+    }
+}
