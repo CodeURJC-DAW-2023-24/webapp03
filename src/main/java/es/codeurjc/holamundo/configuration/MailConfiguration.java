89ec0875d44eef9584040dd9f5816ab912736c3b
Etheko <i.ruizba.2021@alumnos.urjc.es>
2024-02-29 23:35:57 +0100

    Revert "Removed Gmail credentials"
    This reverts commit f0b24a098fa05fe847298c6f22f54ad8248e6904.

--- src/main/java/es/codeurjc/holamundo/configuration/MailConfiguration.java
+++ src/main/java/es/codeurjc/holamundo/configuration/MailConfiguration.java
@@ -1,6 +1,5 @@
 package es.codeurjc.holamundo.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,20 +10,14 @@ import java.util.Properties;
 @Configuration
 public class MailConfiguration {
 
-    @Value("${GMAIL_USERNAME}")
-    private String username;
-
-    @Value("${GMAIL_PASSWORD}")
-    private String password;
-
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setUsername("bookmarks.mail.service@gmail.com");
+        mailSender.setPassword("kbpb alza ostj bdui");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
