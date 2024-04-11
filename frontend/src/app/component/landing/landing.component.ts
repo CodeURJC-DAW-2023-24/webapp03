eec44a04c2dff4f018448fcf4ed83d4e2aab2f87
Izan Ruiz Ballesteros <117302436+Etheko@users.noreply.github.com>
2024-04-11 10:48:27 +0200

    REST API Proxy implemented
    The proxy configuration for the REST API has been implemented and the API can now be accessed by Angular.

--- frontend/src/app/component/landing/landing.component.ts
+++ frontend/src/app/component/landing/landing.component.ts
@@ -1,10 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: "app-landing",
   templateUrl: "./landing.component.html",
   styleUrls: ["./landing.component.css"]
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
   title = "Bookmarks";
+
+  constructor(private http: HttpClient) {
+  }
+
+
+ngOnInit() {
+    this.http.get("/api/users?query=YourReader&page=0").subscribe(data => {
+      console.log(data);
+    });
+  }
 }
